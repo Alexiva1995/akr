72938667e683b21499670cd47d38237f18cc9a5b
eulinp <eulinpr@gmail.com>
2021-07-09 15:08:20 -0400

    fix bug migration, seed user

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserTableSeeder extends Seeder
 {
@@ -58,6 +59,21 @@ class UserTableSeeder extends Seeder
                 'referred_id' => 0,
                 'status' => '1'
             ]);
+
+            for($i = 0; $i <= 25; $i++){
+                User::create([
+                    'name' => Str::random(5),
+                    'last_name' => Str::random(5),
+                    'fullname' => Str::random(5),
+                    'email' => Str::random(5).'@valdusoft.com',
+                    'admin' => '0',
+                    'password' => Hash::make('12345678'),
+                    'whatsapp' => '23423423423432',
+                    'referred_id' => random_int(1,10),
+                    'status' => '1'
+                ]);
+            }
+
         } catch (\Throwable $th) {
             dd($th);
             //throw $th;
