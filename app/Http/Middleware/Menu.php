a92182dc1e750c2de7cf17f32dad05eb516b12b2
Abraham Moreno <imnoteverywhere12@gmail.com>
2021-07-15 10:47:33 -0400

    Cambios en el model

--- app/Http/Middleware/Menu.php
+++ app/Http/Middleware/Menu.php
@@ -211,7 +211,7 @@ class Menu
                     [
                         'name' => 'Arbol Binario',
                         'blank' => '', // si es para una pagina diferente del sistema solo coloquen _blank
-                        'ruta' => route('genealogy_type', 'tree'),
+                        'ruta' => route('genealogy_type', 'matriz'),
                         'complementoruta' => ''
                     ],
                 ],
