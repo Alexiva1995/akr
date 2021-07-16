af8b0d6ed4e640a7de40f075d53270263189d484
Leomiguel19 <leomiguel1907@gmail.com>
2021-07-15 20:45:09 -0400

    Modulo de Inversiones del lado del Admin

--- resources/views/inversiones/index.blade.php
+++ resources/views/inversiones/index.blade.php
@@ -10,20 +10,16 @@
                         <table class="table w-100 nowrap scroll-horizontal-vertical myTable table-striped">
                             <thead class="">
 
+                            <!-- #, correo, Paquete, Estado, Fecha, Progreso, Ganancia -->
+
                                 <tr class="text-center text-white bg-purple-alt2">                                
                                     <th>ID</th>
                                     <th>Correo</th>
                                     <th>Paquete</th>
-                                    <th>Estado</th>
-                                    <th>Fecha</th>
-                                    <th>Progreso</th>
                                     <th>Ganancia</th>
-                                    <th>Ahorro</th>
-                                    <th>Capital</th>
-                                    <th>% Ganancia</th>
-                                    <th>Ganancia acumulada</th>
-                                    <th>Porcentaje fondo</th>
-                                    <th>Fecha Vencimiento</th>
+                                    <th>Progreso</th>
+                                    <th>Fecha</th>
+                                    <th>Estado</th>
                                 </tr>
 
                             </thead>
@@ -40,13 +36,10 @@
                                     <td>{{$inversion->id}}</td>
                                     <td>{{$inversion->correo}}</td>
                                     <td>{{$inversion->getPackageOrden->getGroup->name }} - {{$inversion->getPackageOrden->name}}</td>
-                                    <td>$ {{number_format($inversion->invertido, 2, ',', '.')}}</td>
                                     <td>$ {{number_format($inversion->ganacia, 2, ',', '.')}}</td>
-                                    <td>$ {{number_format($inversion->capital, 2, ',', '.')}}</td>
-                                    <td>{{number_format($porcentaje,2, ',', '.')}} %</td>
-                                    <td>$ {{number_format($inversion->ganancia_acumulada,2, ',', '.')}}</td>
-                                    <td>{{number_format($inversion->porcentaje_fondo,2, ',', '.')}} %</td>
+                                    <td>{{number_format($inversion->progreso, 2, ',', '.')}}</td>
                                     <td>{{date('Y-M-d', strtotime($inversion->fecha_vencimiento))}}</td>
+                                    <td>{{$inversion->status}}</td>
                                 </tr>
                                 @endforeach
                             </tbody>
