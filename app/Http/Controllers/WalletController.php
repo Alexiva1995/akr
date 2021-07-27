acc7c3acb175bf692b72cd5ccb1912d82040fd52
Leomiguel19 <leomiguel1907@gmail.com>
2021-07-27 09:14:32 -0400

    Cron del Inversiones

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -363,7 +363,7 @@ class WalletController extends Controller
                 'referred_id' => null,
                 'cierre_comision_id' => null,
                 'monto' => $cantidad,
-                'descripcion' => 'Profit de '.($porcentaje->porcentaje_utilidad * 100). ' %',
+                'descripcion' => 'Profit de '.($porcentaje * 100). ' %',
                 'status' => 0,
                 'tipo_transaction' => 0,
                 'orden_purchases_id' => $inversion->orden_id
