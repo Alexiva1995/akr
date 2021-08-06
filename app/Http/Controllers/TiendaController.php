aea311e60c0685c424d606f14bcffc55cfa12ee0
Abraham Moreno <imnoteverywhere12@gmail.com>
2021-08-06 08:35:40 -0400

    arreglos en el status

--- app/Http/Controllers/TiendaController.php
+++ app/Http/Controllers/TiendaController.php
@@ -307,7 +307,7 @@ class TiendaController extends Controller
                       Crypto_Value::create([
                           'iduser' => $orden->iduser,
                            'cantidad' => $resultDivision,
-                           'status' => $user->status,
+                           'status' => 1,
                           
                        ]);}
                       
