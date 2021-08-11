6c4cabb93759405e10eaf5206de6640d0fb9d9e1
Abraham Moreno <imnoteverywhere12@gmail.com>
2021-08-10 20:25:21 -0400

    Footer del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
 </style>
 @endpush
 
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 
 <body>
 
@@ -131,15 +131,12 @@
             </div>
             </header>
 
-
-
-
         </div>
     </div>
     </div>
     </div>
     </div>
-
+    @include('auth.footer')
 
 </body>
 
