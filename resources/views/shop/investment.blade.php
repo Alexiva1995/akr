84c13b14c2b505753dd4d4bb9573b41f2375461a
Abraham Moreno <imnoteverywhere12@gmail.com>
2021-08-16 14:05:35 -0400

    m

--- resources/views/shop/investment.blade.php
+++ resources/views/shop/investment.blade.php
@@ -52,8 +52,8 @@ color: white;
                             <div class="range-container ">
                                 <h1 class="text-center text-white mt-3 mb-3">Selecciona El Monto A Depositar</h1>
                                 <a href="#" class="btn" style="">Iniciar</a>
-                                <input class="range" type="range" name="range" id="range" min="70" max="10000" value="70" step="1" onmousemove="rangevalue1.value=value" />
-                                <output id="rangevalue1"></output>
+                                <input class="range" type="range" name="range" id="range" min="70" max="10000" value="70" step="1" oninput="num.value = this.value"/>
+                                <output id="num"></output>
                             </div>
                         </div>
                         <br>
@@ -61,9 +61,16 @@ color: white;
                     </div>
         </div>
 
+        <script>
+                $(document).ready(function()){
+                  $(input#range).on()  
+                };
+
+            </script>
+
         <div class="card-body" id="card">
 
-            <h2 class="text-center mt-3 text-white">Total a Pagar : $ 8.000 USD</h2>
+            <h2 class="text-center mt-3 text-white" >Total a Pagar : $ 8.000 USD</h2>
             <p class="text-center mt-1" style="color: #00F6E1;">Debes realizar un pago de 8.000 USD para <br> poder activar tu plan.</p>
 
             @if(isset($user))
@@ -84,6 +91,7 @@ color: white;
                     <button type="submit" class="btn text-dark float-right" style="background-color:#08C5B9;font-weight:600;">PAGAR VÍA COINPAYMENTS</button>
                 </div>
             </div>
+            
         </div>
         </form>
 
