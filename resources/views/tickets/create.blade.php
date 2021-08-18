143755e1361587ee1ef8dca613aad240382eeb5b
Abraham Moreno <imnoteverywhere12@gmail.com>
2021-08-18 10:24:34 -0400

    Cambios del create user

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -5,11 +5,10 @@
 
 <section id="basic-vertical-layouts">
     <div class="row match-height d-flex justify-content-center">
-        <div class="col-md-9 col-12">
-            <div class="card bg-lp">
-                <div class="card-header">
-                    <h4 class="card-title ">Creacion de Ticket</h4>
-                </div>
+        <div class="col-md-12 col-12">
+            <div class="card" style="
+background: linear-gradient(180deg, #0F1522 0%, rgba(15, 21, 34, 0) 100%);
+border-radius: 8px;">
                 <div class="card-content">
                     <div class="card-body">
                         <form action="{{route('ticket.store')}}" method="POST">
@@ -17,13 +16,35 @@
                             <div class="form-body">
                                 <div class="row">
 
-                                    <div class="col-12">
-                                        <label class="form-label  mb-1" for="issue"><b>Asunto del
-                                                ticket</b></label>
-                                        <input class="form-control border  rounded-0" required type="text" id="issue" name="issue" rows="3" />
+                                    <div class="col-6">
+                                        <label id="form-label" class="form-label" for="name"><b>Nombre</b></label>
+                                        <input class="form-control" required type="text" id="name" name="name" rows="3" />
 
                                     </div>
 
+                                    <div class="col-6">
+                                        <label id="form-label" class="form-label" for="email"><b>Direccion de correo electrónico</b></label>
+                                        <input class="form-control" required type="text" id="email" name="email" rows="3" />
+
+                                    </div>
+
+
+
+                                    <div class="col-12">
+                                        <label id="form-label" class="form-label mt-2" for="issue"><b>Sujeto</b></label>
+                                        <input class="form-control" required type="text" id="issue" name="issue" rows="3" />
+
+                                    </div>
+
+
+
+                                    <div class="col-12">
+                                        <button type="submit" id="send" class="col-12 btn  mr-1 mb-1 waves-effect waves-light">Enviar
+                                            Ticket</button>
+                                    </div>
+
+
+                                        <!--
                                     <div class="col-12 mt-2">
                                         <div class="form-group">
                                             <div class="controls">
@@ -38,57 +59,14 @@
                                         </div>
                                     </div>
 
-                                    <div class="col-12 mt-2 mb-2">
-                                        <label class="form-label  mb-1" for="message"><b>Chat con el
-                                                administrador</b></label>
-
-                                        <section class="chat-app-window mb-2 border rounded-0">
-                                            <div class="active-chat">
-                                                <div class="user-chats ps ps--active-y bg-lp">
-                                                    <div class="chats chat-thread">
-
-                                                        {{-- admin --}}
-                                                        <div class="chat chat-left">
-                                                            <div class="chat-avatar">
-                                                                <span class="avatar ">
-                                                                    <img src="{{asset('assets/img/sistema/favicon.png')}}" alt="avatar" height="40" width="40" style="background-color: white;">
-                                                                </span>
-                                                            </div>
-                                                            <div class="chat-body">
-                                                                <div class="chat-content">
-                                                                    <div class="email-admin mb-1">{{$admin}}</div>
-                                                                    <p>¿Cómo podemos ayudarle? </p>
-                                                                </div>
-                                                            </div>
-                                                        </div>
+-->
 
 
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </section>
-
-                                        <br>
-                                        <span class="text-danger text-bold-600">Aqui podra escribir el mensaje para el admin</span>
-                                        <textarea class="form-control border  rounded-0 chat-window-message" type="text" id="message" name="message" required rows="3"></textarea>
-                                    </div>
-
-
-                                    <div class="col-12">
-                                        <button type="submit" class="btn btn-primary mr-1 mb-1 waves-effect waves-light">Enviar
-                                            Ticket</button>
-                                    </div>
-
-                                </div>
-
-
-                            </div>
+                        </form>
                     </div>
-                    </form>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 </section>
 
